fdd7bd2aaf6ebbbf958abff8c0321f04e9b831af
Gargant <gargant0373@gmail.com>
2023-09-06 16:01:57 +0200

    added TA as a job

--- src/components/Career.tsx
+++ src/components/Career.tsx
@@ -4,11 +4,12 @@ function Career() {
     return <div style={{
         display: "flex", flexDirection: "column", alignItems: "center", marginBottom: "25px"
     }}>
-        <CareerDisplay title="highschool" description="i graduated highschool in brasov, romania with 9.4 on the baccalaurate exam." date="2022" />
-        <CareerDisplay title="university" description="i am currently studying computer science and engineering at tu delft. i started studying in 2022 and so far I have developed
-        many skils that I consider very useful for my career." date="2025" />
         <CareerDisplay title="spigot development" description="my passion for coding started with minecraft. me and my friend learned together how to write spigot minecraft plugins in Java." date="2018" />
         <CareerDisplay title="summer camp counselor" description="I spent 11 days along 4 other team members working with the 30 young adult participants towards making them see their own beauty and value." date="2021" />
+        <CareerDisplay title="highschool" description="i graduated highschool in brasov, romania with 9.4 on the baccalaurate exam." date="2022" />
+        <CareerDisplay title="teaching assistant" description="i worked as a TA for the courses Computer Organization, Reasoning and Logic and Object Oriented Programming at TU Delft" date="2023" />
+        <CareerDisplay title="university" description="i am currently studying computer science and engineering at tu delft. i started studying in 2022 and so far I have developed
+        many skils that I consider very useful for my career." date="2025" />
     </div >
 }
 
